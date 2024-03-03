d893c7bd242d2bd0d26567c3eea5b2fd3fa07147
arfa-z25 <143123897+arfa-z25@users.noreply.github.com>
2024-03-03 12:36:57 +0500

    Add files via upload

---
+++ Project_Form.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Cash_Managment
+{
+    partial class Project_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Project_Form));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblp1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lBLp2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblP3 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnviewproject = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.Controls.Add(this.pic1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblp1);
+            this.panel1.Location = new System.Drawing.Point(618, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(396, 168);
+            this.panel1.TabIndex = 0;
+            // 
+            // pic1
+            // 
+            this.pic1.Image = ((System.Drawing.Image)(resources.GetObject("pic1.Image")));
+            this.pic1.Location = new System.Drawing.Point(346, 99);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(35, 35);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic1.TabIndex = 4;
+            this.pic1.TabStop = false;
+            this.pic1.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 110);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(26, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Assign ID";
+            // 
+            // lblp1
+            // 
+            this.lblp1.AutoSize = true;
+            this.lblp1.Font = new System.Drawing.Font("Georgia", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblp1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblp1.Location = new System.Drawing.Point(34, 18);
+            this.lblp1.Name = "lblp1";
+            this.lblp1.Size = new System.Drawing.Size(51, 35);
+            this.lblp1.TabIndex = 0;
+            this.lblp1.Text = "P1";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel2.Controls.Add(this.pic2);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.lBLp2);
+            this.panel2.Location = new System.Drawing.Point(618, 246);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(396, 171);
+            this.panel2.TabIndex = 1;
+            // 
+            // pic2
+            // 
+            this.pic2.Image = ((System.Drawing.Image)(resources.GetObject("pic2.Image")));
+            this.pic2.Location = new System.Drawing.Point(346, 72);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(35, 35);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic2.TabIndex = 4;
+            this.pic2.TabStop = false;
+            this.pic2.Click += new System.EventHandler(this.pic2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(123, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(217, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(20, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Assign ID";
+            // 
+            // lBLp2
+            // 
+            this.lBLp2.AutoSize = true;
+            this.lBLp2.Font = new System.Drawing.Font("Georgia", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lBLp2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lBLp2.Location = new System.Drawing.Point(24, 25);
+            this.lBLp2.Name = "lBLp2";
+            this.lBLp2.Size = new System.Drawing.Size(55, 35);
+            this.lBLp2.TabIndex = 0;
+            this.lBLp2.Text = "P2";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Controls.Add(this.textBox3);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.lblP3);
+            this.panel3.Location = new System.Drawing.Point(618, 445);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(404, 176);
+            this.panel3.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(346, 93);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(123, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(217, 22);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(20, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Assign ID";
+            // 
+            // lblP3
+            // 
+            this.lblP3.AutoSize = true;
+            this.lblP3.Font = new System.Drawing.Font("Georgia", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblP3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblP3.Location = new System.Drawing.Point(30, 31);
+            this.lblP3.Name = "lblP3";
+            this.lblP3.Size = new System.Drawing.Size(55, 35);
+            this.lblP3.TabIndex = 0;
+            this.lblP3.Text = "P3";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(38, 38);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(558, 583);
+            this.dataGridView2.TabIndex = 6;
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(866, 687);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(139, 49);
+            this.btnBack.TabIndex = 19;
+            this.btnBack.Text = "BACK";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnviewproject
+            // 
+            this.btnviewproject.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnviewproject.Font = new System.Drawing.Font("Georgia", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnviewproject.ForeColor = System.Drawing.Color.White;
+            this.btnviewproject.Location = new System.Drawing.Point(38, 627);
+            this.btnviewproject.Name = "btnviewproject";
+            this.btnviewproject.Size = new System.Drawing.Size(173, 70);
+            this.btnviewproject.TabIndex = 20;
+            this.btnviewproject.Text = "View Details";
+            this.btnviewproject.UseVisualStyleBackColor = false;
+            this.btnviewproject.Click += new System.EventHandler(this.btnviewproject_Click);
+            // 
+            // Project_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 760);
+            this.Controls.Add(this.btnviewproject);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Project_Form";
+            this.Text = "Project";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblp1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lBLp2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblP3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnviewproject;
+    }
+}
---
+++ Project_Form.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Cash_Managment.Business_Layer;
+using Cash_Managment.DataLayer;
+using Cash_Managment.Presentation_layer;
+
+namespace Cash_Managment
+{
+    public partial class Project_Form : Form
+    {
+      
+        private string id2;
+      
+
+        public Project_Form()
+        {
+            InitializeComponent();
+        }
+
+        public Project_Form(string id2)
+        {
+            this.id2 = id2;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pic1_Click(object sender, EventArgs e)
+        {
+            string id = textBox1.Text;
+            string id2 = lblp1.Text;
+            Employee e1 = new Employee(id);
+            
+            Project p = new Project(id2);
+            
+            e1.employee_Project(p);
+            Connection.Assign_Project(e1, p);
+            MessageBox.Show("Project is Assigned Succesfully!!");
+        }
+
+        private void lbl1_Click(object sender, EventArgs e)
+        {
+
+        }
+      
+
+       
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            Welcome w1 = new Welcome();
+            w1.Show();
+            this.Hide();
+        }
+
+        private void btnviewproject_Click(object sender, EventArgs e)
+        {
+            dataGridView2.DataSource = Connection.GetProjectData();
+        }
+
+        private void pic2_Click(object sender, EventArgs e)
+        {
+
+            string id = textBox2.Text;
+            string id2 = lBLp2.Text;
+            Employee e1 = new Employee(id);
+
+            Project p = new Project(id2);
+
+            e1.employee_Project(p);
+            Connection.Assign_Project(e1, p);
+            MessageBox.Show("Project is Assigned Succesfully!!");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            string id = textBox3.Text;
+            string id2 = lblP3.Text;
+            Employee e1 = new Employee(id);
+
+            Project p = new Project(id2);
+
+            e1.employee_Project(p);
+            Connection.Assign_Project(e1, p);
+            MessageBox.Show("Project is Assigned Succesfully!!");
+        }
+    }
+   
+}
